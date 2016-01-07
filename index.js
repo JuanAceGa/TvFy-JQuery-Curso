2be046e1531329e22bb0c88d8df8d6be67ae5b7e
Juan Esteban <juan.acevedo.gallego@gmail.com>
2016-01-06 21:06:22 -0500

    Se agrega paquete de dependencias

--- index.js
+++ index.js
@@ -1,98 +1,77 @@
-// alert('Hola Platzi');
-(function (window, document, $, undefined) {
-	(function(){
-		return index = {
-			template: "",
-			tvShowContainer: $("#app-body").find(".tv-shows"),
+/*
+* Module Dependencies
+*/
 
-			init: function() {
-				this.cargarShows();
-				this.buscarShow();
-				this.liked();
-			},
+var $ = require('jquery');
 
-			cargarShows: function() {
-				var self = this;
+$(function(){
+	var template = "";
+	var tvShowContainer = $("#app-body").find(".tv-shows");
 
-				if (!localStorage.shows) {
-					$.ajax("http://api.tvmaze.com/shows")
-						.then(function(shows) {
-							localStorage.shows = JSON.stringify(shows);
-							self.renderShow(shows);
-					});
-					} else {
-						self.renderShow(JSON.parse(localStorage.shows));
-					}
-					
-			},
+	function renderShow(shows) {
+		tvShowContainer.find(".loader").remove();
+		template = '<article class="tv-show">' +
+						'<div class="left img-container">' +
+							'<img src=":img:" alt=":img alt:">' +
+						'</div>' +
+						'<div class="right info">' +
+							'<h1>:name:</h1>' +
+							'<p>:summary:</p>' +
+							'<button class="like">✴</button>' +
+				   '</article>';
 
-			buscarShow: function(){
-				var self = this;
-				var busqueda;
-				var $loader = $('<div class="loader">');
+	    shows.forEach(function (show) {
+	    	var article = template
+	    		.replace(":name:", show.name)
+				.replace(":img:", show.image ? show.image.medium : "")
+				.replace(":summary:", show.summary)
+				.replace(":img alt:", show.name + " Logo");
 
-				$("#app-body")
-					.find("form")
-					.submit(function (ev){
-						ev.preventDefault();
-						
-						busqueda = $(this)
-							.find("input[type='text']")
-							.val();
+			var $article = $(article);
 
-						self.tvShowContainer.find(".tv-show").remove();
-						$loader.appendTo(self.tvShowContainer);
+			tvShowContainer.append($article.fadeIn(2000));
+		});
+	}
 
-						$.ajax({
-							url: "http://api.tvmaze.com/search/shows",
-							data: {q: busqueda},
-							success: function(res, textStatus, xhr) {
-								$loader.remove();
-								var shows = res.map(function(el) {
-									return el.show;
-								});
-								self.renderShow(shows);
-							}
-						});
-					});
-			},
+	if (!localStorage.shows) {
+		$.ajax("http://api.tvmaze.com/shows")
+			.then(function(shows) {
+				localStorage.shows = JSON.stringify(shows);
+				renderShow(shows);
+			});
+	} else {
+		renderShow(JSON.parse(localStorage.shows));
+	}
+		
 
-			renderShow: function(shows){
-				var self = this;
+	$("#app-body").find("form").submit(function (ev) {
+		ev.preventDefault();
 
-				self.tvShowContainer.find(".loader").remove();
-				self.template = '<article class="tv-show">' +
-									'<div class="left img-container">' +
-										'<img src=":img:" alt=":img alt:">' +
-									'</div>' +
-									'<div class="right info">' +
-										'<h1>:name:</h1>' +
-										'<p>:summary:</p>' +
-										'<button class="like">✴</button>' +
- 								'</article>';
+		var busqueda;
+		var $loader = $('<div class="loader">');
 
-				shows.forEach(function (show) {
-					var article = self.template
-						.replace(":name:", show.name)
-						.replace(":img:", show.image ? show.image.medium : "")
-						.replace(":summary:", show.summary)
-						.replace(":img alt:", show.name + " Logo");
+		busqueda = $(this).find("input[type='text']").val();
 
-						var $article = $(article);
+		tvShowContainer.find(".tv-show").remove();
+		$loader.appendTo(tvShowContainer);
 
-						self.tvShowContainer.append($article.fadeIn(2000));
+		$.ajax({
+			url: "http://api.tvmaze.com/search/shows",
+			data: {q: busqueda},
+			success: function(res, textStatus, xhr) {
+				$loader.remove();
+				var shows = res.map(function(el) {
+					return el.show;
 				});
-			},
-
-			liked: function (){
-				var self = this;
-
-				self.tvShowContainer.on("click", "button.like", function (ev) {
-					var $this = $(this);
-					$this.closest(".tv-show").toggleClass("liked");
-				});
+				renderShow(shows);
 			}
-		}
-	})();
-	index.init();
-})(window, document, jQuery);
+		});
+	});
+
+		
+	tvShowContainer.on("click", "button.like", function (ev) {
+		var $this = $(this);
+		$this.closest(".tv-show").toggleClass("liked");
+	});
+
+})
